f575b0eaaec538c97118bf6d8c34f62086a3fd09
digitsu <digitsu@gmail.com>
2024-03-04 16:45:46 +0900

    init

---
+++ lib/weather_tracker_web/router.ex
@@ -0,0 +1,14 @@
+defmodule WeatherTrackerWeb.Router do
+  use WeatherTrackerWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", WeatherTrackerWeb do
+    pipe_through :api
+
+    post "/weather-conditions", WeatherConditionsController, :create
+  end
+
+end
